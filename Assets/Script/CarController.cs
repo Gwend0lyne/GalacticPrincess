fbf17374cc7cae400ce78691fd3142c5d5dce793
Gwend0lyne <gwendolyne.bouchard@etu.univ-cotedazur.fr>
2025-02-05 10:54:05 +0100

    Initial commit

---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody rg;
+    public float forwardMoveSpeed;
+    public float backwardMoveSpeed;
+    public float steerSpeed;
+
+    private float inputX;
+    private float inputY;
+    void Update() // Get keyboard inputs
+    {
+        inputY = Input.GetAxis("Vertical");
+        inputX = Input.GetAxis("Horizontal");
+    }
+   
+    void FixedUpdate() // Apply physics here
+    {
+        float speed = inputY > 0 ? forwardMoveSpeed : backwardMoveSpeed;
+        if (inputY == 0) speed = 0;
+        rg.AddForce(this.transform.right * speed, ForceMode.Acceleration);
+        // Steer
+        float rotation = inputX * steerSpeed * Time.fixedDeltaTime;
+        transform.Rotate(0, rotation, 0, Space.World);
+    }
+}
